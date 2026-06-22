dfa4f88d69e4a93f76516d371c4b9dc32c3e243e
haphungw <haphung@google.com>
2026-06-22 17:31:54 +0000

    remove redundant condition

--- internal/sidekick/rust/codec.go
+++ internal/sidekick/rust/codec.go
@@ -385,7 +385,7 @@ func resolveUsedPackages(model *api.API, extraPackages []*packagez) {
 		// not save us any computations.
 
 		for _, m := range s.Methods {
-			if m.OperationInfo != nil || m.DiscoveryLro != nil || m.IsLroPoller {
+			if m.OperationInfo != nil || m.IsLroPoller {
 				hasLROs = true
 			}
 			if len(m.AutoPopulated) != 0 {
